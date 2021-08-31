f7e6785a94a5541793dccb2c566892c0f5148dec
isansonetti-payall <isansonetti@payall.com.ve>
2021-08-31 10:55:48 -0400

    first commit

---
+++ src/main/java/com/practica/springboot/practicaSpringBoot/PracticaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.practica.springboot.practicaSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practica/springboot/practicaSpringBoot/PracticaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practica.springboot.practicaSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
